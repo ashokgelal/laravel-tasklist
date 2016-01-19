cf52908cc956771075462e455af2baaf767b2980
Ashok Gelal <ashokgelal@gmail.com>
2016-01-19 00:59:45 -0700

    Completed

--- database/migrations/2016_01_19_061533_create_task_table.php
+++ database/migrations/2016_01_19_061533_create_task_table.php
@@ -14,6 +14,7 @@ class CreateTaskTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
